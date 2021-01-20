84ebeadf11b8eac82d9394496921ef4ab64b7c12
Sanjay T <59244273+sanjay-06@users.noreply.github.com>
2021-01-21 00:11:47 +0530

    Delete psb2_1.py

--- sem4/python/19PW28 Problem sheet basics 2/psb2_1.py
+++
@@ -1,9 +0,0 @@
-sentence=input("Enter the sentence: ").split(" ")
-checksentence=[]
-for i in sentence:
-    if i not in checksentence:
-       checksentence.append(i)
-    else:
-       pass
-checksentence.sort()
-print("Output is : {}".format(" ".join(checksentence)))
